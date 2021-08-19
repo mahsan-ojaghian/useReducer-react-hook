5d04eafb2d5b62790d1a48aef684b314812fd793
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-19 19:11:10 +0430

    initial state

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,13 @@ const initialState = {
   card: {
     price: 26395,
     name: "2019 Ford",
-    img: ""
-  }
+    img:
+      "https://www.autoevolution.com/cars/ford-f-150-raptor-2021.html#aegal_0",
+    features: []
+  },
+  store: [
+    { id: 1, name: "v-6", price: 1500 },
+    { id: 2, name: "Racing", price: 1500 },
+    { id: 3, name: "Rear", price: 1500 }
+  ]
 };
